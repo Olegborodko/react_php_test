78a93aa0d46ff7117ca1cfc4f8d6ff6393cb0c15
borodkoleg <borodkoleg@gmail.com>
2023-10-27 16:09:35 +0300

    base structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CategoryPage from './pages/CategoryPage';
+import ProductPage from './pages/ProductPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<CategoryPage />} />
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/CategoryPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CategoryPage = () => {
+  return <div>Category Page</div>;
+};
+
+export default CategoryPage;
